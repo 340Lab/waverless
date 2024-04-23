b42f349738b4f92596d7b7a16c465e44284b924c
ActivePeter <1020401660@qq.com>
2024-04-23 13:26:51 +0000

    scripts: install basic

---
+++ scripts/install/2.ans_install_build.py
@@ -0,0 +1,9 @@
+
+### chdir
+import os
+CUR_FPATH = os.path.abspath(__file__)
+CUR_FDIR = os.path.dirname(CUR_FPATH)
+# chdir to the directory of this script
+os.chdir(CUR_FDIR)
+
+os.system('ansible-playbook -vv 2.ans_install_build.yml -i ../local_ansible_conf.ini')
